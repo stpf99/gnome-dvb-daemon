4475ebd3af5799fd5bfe9e7dd6e6573d62de5bf7
Sebastian Pölsterl <marduk@k-d-w.org>
2008-07-24 18:54:35 +0200

    Registered and unregistered devices are displayed. There's still no functionality.

--- client/preferences.py
+++ client/preferences.py
@@ -3,14 +3,34 @@
 import gtk
 import gobject
 import gnomedvb
+import re
+
+PRIME = 31
 
 class Device:
 
-    pass
+    def __init__(self, name, adapter, frontend, devtype):
+        self.name = name
+        self.adapter = adapter
+        self.frontend = frontend
+        self.type = devtype
+        
+    def __hash__(self):
+        return 2 * PRIME + PRIME * self.adapter + self.frontend
+        
+    def __eq__(self, other):
+        if not isinstance(other, Device):
+            return False
+        
+        return (self.adapter == other.adapter \
+            and self.frontend == other.frontend)
+            
+    def __repr__(self):
+        return "/dev/dvb/adapter%d/frontend%d" % (self.adapter, self.frontend)
 
 class UnassignedDevicesStore (gtk.ListStore):
 
-    COL_DEVICE = range(1)
+    (COL_DEVICE,) = range(1)
     
     def __init__(self):
         gtk.ListStore.__init__(self, gobject.TYPE_PYOBJECT)
@@ -18,7 +38,7 @@ class UnassignedDevicesStore (gtk.ListStore):
 
 class DeviceGroupsStore (gtk.TreeStore):
 
-    COL_DEVICE = range(1)
+    (COL_DEVICE,) = range(1)
 
     def __init__(self):
         gtk.TreeStore.__init__(self, gobject.TYPE_PYOBJECT)
@@ -28,14 +48,25 @@ class DeviceGroupsView (gtk.TreeView):
 
     def __init__(self, model):
         gtk.TreeView.__init__(self, model)
+        self.set_headers_visible(False)
+        self.set_reorderable(True)
         
         cell_description = gtk.CellRendererText ()
-        column_description = gtk.TreeViewColumn ("Description", cell_description)
+        column_description = gtk.TreeViewColumn ("Devices", cell_description)
         column_description.set_cell_data_func(cell_description, self.get_description_data)
+        self.append_column(column_description)
         
     def get_description_data(self, column, cell, model, aiter):
         device = model[aiter][model.COL_DEVICE]
-
+        
+        if isinstance(device, Device):
+            text = "<b>%s (%s)</b>\n" % (device.name, device.type)
+            text += "<small>Adapter: %d, Frontend: %d</small>" % (device.adapter,
+                device.frontend)
+        else:
+            text = "Group %d" % device
+            
+        cell.set_property("markup", text)
 
 class AlignedLabel (gtk.Alignment):
 
@@ -79,10 +110,12 @@ class DVBPreferences(gtk.Window):
     def __init__(self):
         gtk.Window.__init__(self)
         
+        self._model = DVBModel()
+        
         self.connect('delete-event', gtk.main_quit)
         self.connect('destroy-event', gtk.main_quit)
         self.set_title("Configure DVB")
-        self.set_default_size(400, 250)
+        self.set_default_size(600, 450)
         self.set_border_width(6)
         
         self.vbox = gtk.VBox(spacing=12)
@@ -98,7 +131,7 @@ class DVBPreferences(gtk.Window):
         self.vbox.pack_start(groups_frame)
 
         self.unassigned_devices = UnassignedDevicesStore()
-        self.unassigned_view = gtk.TreeView(self.unassigned_devices)
+        self.unassigned_view = DeviceGroupsView(self.unassigned_devices)
         self.unassigned_view.show()
         
         unassigned_frame = Frame("<b>Unassigned devices</b>", self.unassigned_view)
@@ -108,7 +141,7 @@ class DVBPreferences(gtk.Window):
         buttonbox = gtk.HButtonBox()
         buttonbox.set_layout(gtk.BUTTONBOX_END)
         buttonbox.show()
-        self.vbox.pack_end(buttonbox)
+        self.vbox.pack_end(buttonbox, False, False, 0)
         
         close_button = gtk.Button(stock=gtk.STOCK_CLOSE)
         close_button.connect('clicked', gtk.main_quit)
@@ -117,7 +150,75 @@ class DVBPreferences(gtk.Window):
         
         separator = gtk.HSeparator()
         separator.show()
-        self.vbox.pack_end(separator)
+        self.vbox.pack_end(separator, False, False, 0)
+        
+        self._fill()
+        
+    def _fill(self):
+        for device in self._model.get_unregistered_devices():
+            self.unassigned_devices.append([device])
+        
+        for group_id, group in self._model.get_registered_device_groups().items():
+            group_iter = self.devicegroups.append(None)
+            self.devicegroups.set(group_iter, self.devicegroups.COL_DEVICE, group_id)
+            
+            for device in group:
+                dev_iter = self.devicegroups.append(group_iter)
+                self.devicegroups.set(dev_iter, self.devicegroups.COL_DEVICE, device)
+
+class DVBModel:
+
+    def __init__(self):
+        self._manager = gnomedvb.DVBManagerClient()
+        self._adapter_pattern = re.compile("adapter(\d+?)/frontend(\d+?)")
+        
+    def get_registered_device_groups(self):
+        """
+        @returns: dict of list of Device
+        """
+        groups = {}
+        for group_id in self._manager.get_registered_device_groups():
+            group = []
+            for device_path in self._manager.get_device_group_members(group_id):
+                match = self._adapter_pattern.search(device_path)
+                if match != None:
+                    adapter = int(match.group(1))
+                    info = gnomedvb.get_adapter_info(adapter)
+                    frontend = int(match.group(2))
+                    dev = Device (info["name"], adapter, frontend, info["type"])
+                    group.append(dev)
+                
+            groups[group_id] = group
+            
+        return groups
+        
+    def get_all_devices(self):
+        """
+        @returns: list of Device
+        """
+        devs = []
+        for info in gnomedvb.get_dvb_devices():
+            dev = Device (info["name"], info["adapter"], info["frontend"],
+                info["type"])
+            devs.append(dev)
+        return devs
+        
+    def get_unregistered_devices(self):
+        """
+        @returns: set of Device
+        """
+        devgroups = self.get_registered_device_groups()
+        registered = set()
+        for group in devgroups.values():
+            for dev in group:
+                registered.add(dev)
+                
+        alldevs = set()
+        for dev in self.get_all_devices():
+            alldevs.add(dev)
+        
+        return alldevs - registered
+        
 
 if __name__=='__main__':
     prefs = DVBPreferences()
