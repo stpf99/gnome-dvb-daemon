917fe143435967c584cfe1e63ae62b8d22edd426
Sebastian Pölsterl <marduk@k-d-w.org>
2008-07-25 15:41:00 +0200

    Added possibility to remove device from group and to create a new group. However, the change is only visible after restart.

--- client/preferences.py
+++ client/preferences.py
@@ -9,7 +9,8 @@ PRIME = 31
 
 class Device:
 
-    def __init__(self, name, adapter, frontend, devtype):
+    def __init__(self, group_id, name, adapter, frontend, devtype):
+        self.group = group_id
         self.name = name
         self.adapter = adapter
         self.frontend = frontend
@@ -27,6 +28,7 @@ class Device:
             
     def __repr__(self):
         return "/dev/dvb/adapter%d/frontend%d" % (self.adapter, self.frontend)
+
 
 class UnassignedDevicesStore (gtk.ListStore):
 
@@ -68,6 +70,7 @@ class DeviceGroupsView (gtk.TreeView):
             
         cell.set_property("markup", text)
 
+
 class AlignedLabel (gtk.Alignment):
 
     def __init__(self, markup):
@@ -77,6 +80,7 @@ class AlignedLabel (gtk.Alignment):
         self.label.set_markup(markup)
         self.label.show()
         self.add(self.label)
+
 
 class AlignedScrolledWindow (gtk.Alignment):
 
@@ -94,23 +98,95 @@ class AlignedScrolledWindow (gtk.Alignment):
 
 class Frame (gtk.VBox):
 
-    def __init__(self, markup, child):
+    def __init__(self, markup, child, buttonbox):
         gtk.VBox.__init__(self, spacing=6)
     
         label = AlignedLabel(markup)
         label.show()
         self.pack_start(label, False, False, 0)
         
+        self.hbox = gtk.HBox(spacing=6)
+        self.hbox.show()
+        self.pack_start(self.hbox)
+        
         view = AlignedScrolledWindow(child)
         view.show()
-        self.pack_start(view)
+        self.hbox.pack_start(view)
+        
+        self.hbox.pack_start(buttonbox, False, False, 0)
 
+
+class NewGroupDialog (gtk.Dialog):
+
+    def __init__(self, parent):
+        gtk.Dialog.__init__(self, title="Create Group",
+            parent=parent,
+            flags=gtk.DIALOG_MODAL | gtk.DIALOG_DESTROY_WITH_PARENT,
+            buttons=(gtk.STOCK_CANCEL, gtk.RESPONSE_REJECT,
+                      gtk.STOCK_OK, gtk.RESPONSE_ACCEPT))
+            
+        table = gtk.Table(rows=2, columns=3)
+        table.set_row_spacings(12)
+        table.set_col_spacings(6)
+        table.show()
+        self.vbox.pack_start(table)
+        
+        channels = gtk.Label()
+        channels.set_markup("<b>Channels File:</b>")
+        channels.show()
+        table.attach(channels, 0, 1, 0, 1, xoptions=0, yoptions=0)
+        
+        self.channels_entry = gtk.Entry()
+        self.channels_entry.set_editable(False)
+        self.channels_entry.show()
+        table.attach(self.channels_entry, 1, 2, 0, 1)
+        
+        channels_open = gtk.Button(stock=gtk.STOCK_OPEN)
+        channels_open.connect("clicked", self._on_channels_open_clicked)
+        channels_open.show()
+        table.attach(channels_open, 2, 3, 0, 1, xoptions=0, yoptions=0)
+        
+        recordings = gtk.Label()
+        recordings.set_markup("<b>Recordings' Directory:</b>")
+        recordings.show()
+        table.attach(recordings, 0, 1, 1, 2, xoptions=0, yoptions=0)
+        
+        self.recordings_entry = gtk.Entry()
+        self.recordings_entry.set_editable(False)
+        self.recordings_entry.show()
+        table.attach(self.recordings_entry, 1, 2, 1, 2)
+        
+        recordings_open = gtk.Button(stock=gtk.STOCK_OPEN)
+        recordings_open.connect("clicked", self._on_recordings_open_clicked)
+        recordings_open.show()
+        table.attach(recordings_open, 2, 3, 1, 2, xoptions=0, yoptions=0)
+        
+    def _on_channels_open_clicked(self, button):
+        dialog = gtk.FileChooserDialog (title = "Select File",
+            parent=self, action=gtk.FILE_CHOOSER_ACTION_OPEN,
+            buttons=(gtk.STOCK_CANCEL, gtk.RESPONSE_REJECT,
+                      gtk.STOCK_OK, gtk.RESPONSE_ACCEPT))
+        if dialog.run() == gtk.RESPONSE_ACCEPT:
+            self.channels_entry.set_text(dialog.get_filename())
+        dialog.destroy()
+    
+    def _on_recordings_open_clicked(self, button):
+        dialog = gtk.FileChooserDialog (title = "Select Directory",
+            parent=self, action=gtk.FILE_CHOOSER_ACTION_SELECT_FOLDER,
+            buttons=(gtk.STOCK_CANCEL, gtk.RESPONSE_REJECT,
+                      gtk.STOCK_OK, gtk.RESPONSE_ACCEPT))
+        if dialog.run() == gtk.RESPONSE_ACCEPT:
+            self.recordings_entry.set_text(dialog.get_filename())
+        dialog.destroy()
+         
 class DVBPreferences(gtk.Window):
 
     def __init__(self):
         gtk.Window.__init__(self)
         
         self._model = DVBModel()
+        self._model.connect("changed", self._on_manager_changed)
+        self._model.connect("group-changed", self._on_group_changed)
         
         self.connect('delete-event', gtk.main_quit)
         self.connect('destroy-event', gtk.main_quit)
@@ -122,21 +198,8 @@ class DVBPreferences(gtk.Window):
         self.add(self.vbox)
         self.vbox.show()
         
-        self.devicegroups = DeviceGroupsStore()
-        self.devicegroupsview = DeviceGroupsView(self.devicegroups)
-        self.devicegroupsview.show()
-        
-        groups_frame = Frame("<b>Registered groups</b>", self.devicegroupsview)
-        groups_frame.show()
-        self.vbox.pack_start(groups_frame)
-
-        self.unassigned_devices = UnassignedDevicesStore()
-        self.unassigned_view = DeviceGroupsView(self.unassigned_devices)
-        self.unassigned_view.show()
-        
-        unassigned_frame = Frame("<b>Unassigned devices</b>", self.unassigned_view)
-        unassigned_frame.show()
-        self.vbox.pack_start(unassigned_frame)
+        self.__create_registered_groups()
+        self.__create_unassigned_devices()
         
         buttonbox = gtk.HButtonBox()
         buttonbox.set_layout(gtk.BUTTONBOX_END)
@@ -154,6 +217,57 @@ class DVBPreferences(gtk.Window):
         
         self._fill()
         
+    def __create_registered_groups(self):
+        self.groups_box = gtk.HBox(spacing=6)
+        self.groups_box.show()
+        self.vbox.pack_start(self.groups_box)
+    
+        self.devicegroups = DeviceGroupsStore()
+        self.devicegroupsview = DeviceGroupsView(self.devicegroups)
+        self.devicegroupsview.get_selection().connect("changed", self._on_groups_selection_changed)
+        self.devicegroupsview.show()
+        
+        buttonbox = gtk.VButtonBox()
+        buttonbox.set_layout(gtk.BUTTONBOX_START)
+        buttonbox.show()
+        
+        self.button_remove = gtk.Button(label = "Remove")
+        self.button_remove.connect("clicked", self._on_button_remove_clicked)
+        self.button_remove.set_tooltip_markup("Remove selected device or group")
+        delete_image = gtk.image_new_from_stock(gtk.STOCK_DELETE, gtk.ICON_SIZE_BUTTON)
+        self.button_remove.set_image(delete_image)
+        self.button_remove.set_sensitive(False)
+        self.button_remove.show()
+        buttonbox.pack_start(self.button_remove)
+        
+        groups_frame = Frame("<b>Registered groups</b>", self.devicegroupsview,
+            buttonbox)
+        groups_frame.show()
+        self.groups_box.pack_start(groups_frame)
+
+    def __create_unassigned_devices(self):
+        self.unassigned_devices = UnassignedDevicesStore()
+        self.unassigned_view = DeviceGroupsView(self.unassigned_devices)
+        self.unassigned_view.get_selection().connect("changed",
+            self._on_unassigned_selection_changed)
+        self.unassigned_view.show()
+        
+        buttonbox = gtk.VButtonBox()
+        buttonbox.set_layout(gtk.BUTTONBOX_START)
+        buttonbox.show()
+        
+        self.button_new = gtk.Button(stock=gtk.STOCK_NEW)
+        self.button_new.connect("clicked", self._on_button_new_clicked)
+        self.button_new.set_tooltip_markup("Create new group for selected device")
+        self.button_new.set_sensitive(False)
+        self.button_new.show()
+        buttonbox.pack_start(self.button_new)
+        
+        unassigned_frame = Frame("<b>Unassigned devices</b>", self.unassigned_view,
+            buttonbox)
+        unassigned_frame.show()
+        self.vbox.pack_start(unassigned_frame)
+        
     def _fill(self):
         for device in self._model.get_unregistered_devices():
             self.unassigned_devices.append([device])
@@ -166,10 +280,56 @@ class DVBPreferences(gtk.Window):
                 dev_iter = self.devicegroups.append(group_iter)
                 self.devicegroups.set(dev_iter, self.devicegroups.COL_DEVICE, device)
 
-class DVBModel:
+    def _on_groups_selection_changed(self, treeselection):
+        model, aiter = treeselection.get_selected()
+        
+        self.button_remove.set_sensitive(aiter != None)
+
+    def _on_unassigned_selection_changed(self, treeselection):
+        model, aiter = treeselection.get_selected()
+        
+        self.button_new.set_sensitive(aiter != None)
+
+    def _on_button_remove_clicked(self, button):
+        model, aiter = self.devicegroupsview.get_selection().get_selected()
+        
+        if aiter != None:
+            device = model[aiter][model.COL_DEVICE]
+            if isinstance(device, Device):
+                if self._model.remove_device_from_group(device):
+                    print "Success: remove device"
+                else:
+                    print "Error: remove device"
+            else:
+                pass
+
+    def _on_button_new_clicked(self, button):
+        model, aiter = self.unassigned_view.get_selection().get_selected()
+        
+        if aiter != None:
+            device = model[aiter][model.COL_DEVICE]
+            dialog = NewGroupDialog(self)
+            if dialog.run() == gtk.RESPONSE_ACCEPT:
+                channels = dialog.channels_entry.get_text()
+                recdir = dialog.recordings_entry.get_text()
+                if self._model.add_device_to_new_group(device.adapter,
+                        device.frontend, channels, recdir):
+                    print "Success: create group"
+                else:
+                    print "Error: create group"
+            dialog.destroy()
+
+    def _on_manager_changed(self, manager, group_id, change_type):
+        pass
+        
+    def _on_group_changed(self, manager, group_id, adapter, frontend, change_type):
+        pass
+
+
+class DVBModel (gnomedvb.DVBManagerClient):
 
     def __init__(self):
-        self._manager = gnomedvb.DVBManagerClient()
+        gnomedvb.DVBManagerClient.__init__(self)
         self._adapter_pattern = re.compile("adapter(\d+?)/frontend(\d+?)")
         
     def get_registered_device_groups(self):
@@ -177,15 +337,15 @@ class DVBModel:
         @returns: dict of list of Device
         """
         groups = {}
-        for group_id in self._manager.get_registered_device_groups():
+        for group_id in gnomedvb.DVBManagerClient.get_registered_device_groups(self):
             group = []
-            for device_path in self._manager.get_device_group_members(group_id):
+            for device_path in self.get_device_group_members(group_id):
                 match = self._adapter_pattern.search(device_path)
                 if match != None:
                     adapter = int(match.group(1))
                     info = gnomedvb.get_adapter_info(adapter)
                     frontend = int(match.group(2))
-                    dev = Device (info["name"], adapter, frontend, info["type"])
+                    dev = Device (group_id, info["name"], adapter, frontend, info["type"])
                     group.append(dev)
                 
             groups[group_id] = group
@@ -198,7 +358,7 @@ class DVBModel:
         """
         devs = []
         for info in gnomedvb.get_dvb_devices():
-            dev = Device (info["name"], info["adapter"], info["frontend"],
+            dev = Device (0, info["name"], info["adapter"], info["frontend"],
                 info["type"])
             devs.append(dev)
         return devs
@@ -219,6 +379,9 @@ class DVBModel:
         
         return alldevs - registered
         
+    def remove_device_from_group(self, device):
+        return gnomedvb.DVBManagerClient.remove_device_from_group(self, device.adapter,
+            device.frontend, device.group)
 
 if __name__=='__main__':
     prefs = DVBPreferences()
