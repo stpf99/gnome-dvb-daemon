263c170f306dca0f96ca9f9d071af549932c4518
Sebastian Pölsterl <marduk@k-d-w.org>
2008-07-27 23:31:59 +0200

    Change UI when group has been added or removed

--- client/preferences.py
+++ client/preferences.py
@@ -412,8 +412,11 @@ class DVBPreferences(gtk.Window):
         # A group has been added or deleted
         if change_type == 0:
             # Added
-            # TODO
-            pass
+            group_iter = self.devicegroups.append(None)
+            self.devicegroups.set(group_iter, self.devicegroups.COL_DEVICE, group_id)
+            for device in self._model.get_device_group_members(group_id):
+                dev_iter = self.devicegroups.append(group_iter)
+                self.devicegroups.set(dev_iter, self.devicegroups.COL_DEVICE, device)
         elif change_type == 1:
             # Removed
             aiter = self.devicegroups.get_iter_first()
@@ -459,17 +462,7 @@ class DVBModel (gnomedvb.DVBManagerClient):
         """
         groups = {}
         for group_id in gnomedvb.DVBManagerClient.get_registered_device_groups(self):
-            group = []
-            for device_path in self.get_device_group_members(group_id):
-                match = self._adapter_pattern.search(device_path)
-                if match != None:
-                    adapter = int(match.group(1))
-                    info = gnomedvb.get_adapter_info(adapter)
-                    frontend = int(match.group(2))
-                    dev = Device (group_id, info["name"], adapter, frontend, info["type"])
-                    group.append(dev)
-                
-            groups[group_id] = group
+            groups[group_id] = self.get_device_group_members(group_id)
             
         return groups
         
@@ -503,6 +496,19 @@ class DVBModel (gnomedvb.DVBManagerClient):
     def remove_device_from_group(self, device):
         return gnomedvb.DVBManagerClient.remove_device_from_group(self, device.adapter,
             device.frontend, device.group)
+            
+    def get_device_group_members(self, group_id):
+        devices = []
+        for device_path in gnomedvb.DVBManagerClient.get_device_group_members(self, group_id):
+            match = self._adapter_pattern.search(device_path)
+            if match != None:
+                adapter = int(match.group(1))
+                info = gnomedvb.get_adapter_info(adapter)
+                frontend = int(match.group(2))
+                dev = Device (group_id, info["name"], adapter, frontend, info["type"])
+                devices.append(dev)
+        return devices
+
 
 if __name__=='__main__':
     prefs = DVBPreferences()
