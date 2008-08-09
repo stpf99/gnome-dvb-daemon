d91abb60ede81a5d96a143a8206bcee5785a57ad
Sebastian <sebastian@ubuntu>
2008-08-09 20:48:03 +0200

    Adjust view to recording-(started|finished) and changed signals of Recorder. Ask for confirmation when aborting active recording. Show record image in front of item when the timer belongs to an active recording.

--- client/gnomedvb/timers/ui/RecorderWindow.py
+++ client/gnomedvb/timers/ui/RecorderWindow.py
@@ -9,9 +9,11 @@ class RecorderWindow(gtk.Window):
     (COL_ID,
     COL_CHANNEL,
     COL_START,
-    COL_DURATION) = range(4)
+    COL_DURATION,
+    COL_ACTIVE,) = range(5)
     
-    (COL_NAME, COL_PATH,) = range(2)
+    (COL_NAME,
+    COL_PATH,) = range(2)
 
     def __init__(self):
         gtk.Window.__init__(self)
@@ -51,13 +53,21 @@ class RecorderWindow(gtk.Window):
         timers_label.set_markup(_("<b>Scheduled recordings:</b>"))
         timers_ali.add(timers_label)
         
-        self.timerslist = gtk.ListStore(int, str, str, int)
+        self.timerslist = gtk.ListStore(int, str, str, int, bool)
         self.timerslist.set_sort_column_id(self.COL_START, gtk.SORT_ASCENDING)
         
         self.timersview = gtk.TreeView(self.timerslist)
         self.timersview.get_selection().connect("changed",
             self._on_timers_selection_changed)
         
+        cell_rec = gtk.CellRendererPixbuf()
+        col_rec = gtk.TreeViewColumn()
+        col_rec.pack_start(cell_rec)
+        col_rec.set_cell_data_func(cell_rec, self._get_recording_icon_for_cell)
+        col_rec.add_attribute(cell_rec, "stock-id", self.COL_ACTIVE)
+        
+        self.timersview.append_column(col_rec)
+         
         cell_id = gtk.CellRendererText()
         col_id = gtk.TreeViewColumn(_("ID"))
         col_id.pack_start(cell_id)
@@ -114,10 +124,12 @@ class RecorderWindow(gtk.Window):
             group_name = _("Group %d") % group_id
             self.recorderslist.append([group_name, group_id])
             self.recorders[group_id] = gnomedvb.DVBRecorderClient(group_id)
+            self.recorders[group_id].connect("changed", self._on_recorder_changed)
+            self.recorders[group_id].connect("recording-started", self._set_recording_state, True)
+            self.recorders[group_id].connect("recording-finished", self._set_recording_state, False)
             
     def get_timers(self, recorder_path):
         rec = self.recorders[recorder_path]
-        rec.connect("changed", self._on_recorder_changed)
         
         for timer_id in rec.get_timers():
             self._add_timer(rec, timer_id)
@@ -128,15 +140,32 @@ class RecorderWindow(gtk.Window):
                 start_list[2], start_list[3], start_list[4])
         duration = rec.get_duration(timer_id)
         channel = rec.get_channel_name(timer_id)
+        active = rec.is_timer_active(timer_id)
         
-        self.timerslist.append([timer_id, channel, starttime, duration])
+        self.timerslist.append([timer_id, channel, starttime, duration, active])
+
+    def _remove_timer(self, timer_id):
+        for row in self.timerslist:
+            if row[self.COL_ID] == timer_id:
+                self.timerslist.remove(row.iter)
 
     def _on_button_delete_clicked(self, button):
         model, aiter = self.timersview.get_selection().get_selected()
         if aiter != None:
             rec = self.recorders[self._get_active_device_group()]
-            rec.delete_timer(model[aiter][self.COL_ID])
-            model.remove(aiter)
+            timer_id = model[aiter][self.COL_ID]
+            if rec.is_timer_active(timer_id):
+                dialog = gtk.MessageDialog(parent=self,
+                    flags=gtk.DIALOG_MODAL|gtk.DIALOG_DESTROY_WITH_PARENT,
+                    type=gtk.MESSAGE_QUESTION, buttons=gtk.BUTTONS_YES_NO)
+                dialog.set_markup(_("Abort active recording?"))
+                dialog.format_secondary_text(
+                    _("The timer you selected belongs to a currently active recording.") + " " +
+                    _("Deleting this timer will abort the recording."))
+                response = dialog.run()
+                dialog.destroy()
+                if response == gtk.RESPONSE_YES:
+                    rec.delete_timer(timer_id)
         
     def _on_button_add_clicked(self, button):   
         device_group = self._get_active_device_group()
@@ -159,20 +188,23 @@ class RecorderWindow(gtk.Window):
         return self.recorderslist[aiter][self.COL_PATH]
         
     def _on_recorderscombo_changed(self, combo):
+        self.timerslist.clear()
         self.get_timers(self._get_active_device_group())
         self.button_add.set_sensitive(True)
         
     def _on_recorder_changed(self, recorder, timer_id, typeid):
-        if (typeid == 0):
-            # Timer added
-            if recorder.get_path().endswith(str(self._get_active_device_group())):
+        group_id = self._get_active_device_group()
+        if recorder == self.recorders[group_id]:
+            if (typeid == 0):
+                # Timer added
                 self._add_timer(recorder, timer_id)
-        elif (typeid == 1):
-            # Timer deleted
-            pass
-        elif (typeid == 2):
-            # Timer changed
-            pass
+            elif (typeid == 1):
+                # Timer deleted
+                self._remove_timer(timer_id)
+            elif (typeid == 2):
+                # Timer changed
+                self._remove_timer(timer_id)
+                self._add_timer(recorder, timer_id)
             
     def _on_timers_selection_changed(self, treeselection):
         model, aiter = treeselection.get_selected()
@@ -181,3 +213,12 @@ class RecorderWindow(gtk.Window):
         else:
             self.button_delete.set_sensitive(True)
 
+    def _set_recording_state(self, recorder, timer_id, state):
+        for row in self.timerslist:
+            if row[self.COL_ID] == timer_id:
+                self.timerslist.set (row.iter, self.COL_ACTIVE, state)
+                
+    def _get_recording_icon_for_cell(self, column, cell, model, aiter):
+        if model[aiter][self.COL_ACTIVE]:
+            cell.set_property("stock-id", gtk.STOCK_MEDIA_RECORD)
+    
