02bfa3b3cfc21114db4172aade15fc9dd76dad66
Sebastian Pölsterl <marduk@k-d-w.org>
2008-08-24 16:22:41 +0200

    Work around pygtk bug when model is None

--- client/gnomedvb/widgets/ChannelsView.py
+++ client/gnomedvb/widgets/ChannelsView.py
@@ -6,20 +6,23 @@ from ChannelsStore import ChannelsStore
 
 class ChannelsView(gtk.TreeView):
 
-    def __init__(self, model):
+    def __init__(self, model=None):
         """
         @type model: ChannelsStore
         """
-        gtk.TreeView.__init__(self, model)
+        if model != None:
+            gtk.TreeView.__init__(self, model)
+        else:
+            gtk.TreeView.__init__(self)
         
         col_name = gtk.TreeViewColumn(_("Channel"))
         cell_name = gtk.CellRendererText()
         col_name.pack_start(cell_name)
-        col_name.add_attribute(cell_name, "text", model.COL_NAME)
+        col_name.add_attribute(cell_name, "text", ChannelsStore.COL_NAME)
         self.append_column(col_name)
         
-    def set_model(self, model):
-        if isinstance(model, ChannelsStore):
+    def set_model(self, model=None):
+        if model != None and not isinstance(model, ChannelsStore):
             raise TypeError("model must be a ChannelsStore instance")
         gtk.TreeView.set_model(self, model)
         
