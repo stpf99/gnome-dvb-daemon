38abecfdaeb0cc293f1d90624096859b21e83946
Sebastian Pölsterl <sebp@k-d-w.org>
2009-01-25 23:44:56 +0100

    Added menu bar

--- client/gnomedvb/ui/controlcenter/ControlCenterWindow.py
+++ client/gnomedvb/ui/controlcenter/ControlCenterWindow.py
@@ -21,18 +21,21 @@ class ControlCenterWindow(gtk.Window):
         self.connect('destroy-event', gtk.main_quit)
         self.set_title(_("DVB Control Center"))
         self.set_default_size(800, 500)
-        self.set_border_width(3)
         
-        self.vbox_outer = gtk.VBox(spacing=6)
+        self.vbox_outer = gtk.VBox()
         self.vbox_outer.show()
         self.add(self.vbox_outer)
         
+        self.toolbar = None
+        self.vbox_left  = None
+        self.__create_menu()
         self.__create_toolbar()
         
         self.hbox = gtk.HBox(spacing=6)
         self.vbox_outer.pack_start(self.hbox)
         
         self.hpaned = gtk.HPaned()
+        self.hpaned.set_border_width(3)
         self.hpaned.set_position(175)
         self.hbox.pack_start(self.hpaned)
         
@@ -81,29 +84,110 @@ class ControlCenterWindow(gtk.Window):
         self.devgroupscombo.set_active(0)
         self.channelsview.grab_focus()
         
-    def __create_toolbar(self):
-        toolbar = gtk.Toolbar()
-        toolbar.show()
-        self.vbox_outer.pack_start(toolbar, False)
+    def __create_menu(self):
+        ui = '''
+        <menubar name="MenuBar">
+          <menu action="Timers">
+            <menuitem action="EditTimers"/>
+            <separator/>
+            <menuitem action="Quit"/>
+          </menu>
+          <menu action="Edit">
+            <menuitem action="Preferences"/>
+          </menu>
+          <menu action="View">
+            <menuitem action="Channels"/>
+            <menuitem action="Toolbar"/>
+          </menu>
+          <menu action="Help">
+            <menuitem action="About"/>
+          </menu>
+        </menubar>'''
+
+        uimanager = gtk.UIManager()
         
-        edit_image = gtk.image_new_from_stock(gtk.STOCK_FIND_AND_REPLACE, gtk.ICON_SIZE_SMALL_TOOLBAR)
+        # Add the accelerator group to the toplevel window
+        accelgroup = uimanager.get_accel_group()
+        self.add_accel_group(accelgroup)
+        
+        # Create actions
+        actiongroup = gtk.ActionGroup('Root')
+        actiongroup.add_actions([
+            ('Timers', None, _('_Timers')),
+            ('Edit', None, _('_Edit')),
+            ('View', None, _('_View')),
+            ('Help', None, _('Help')),
+        ])
+        # Add the actiongroup to the uimanager
+        uimanager.insert_action_group(actiongroup, 0)
+        
+        actiongroup = gtk.ActionGroup('Timers')
+        actiongroup.add_actions([
+            ('EditTimers', gtk.STOCK_SAVE, _('_Manage'), None,
+             _('Create and edit timers'), self._on_button_display_timers_clicked),
+            ('Quit', gtk.STOCK_QUIT, _('_Quit'), None,
+             _('Quit the Program'), gtk.main_quit)])
+        uimanager.insert_action_group(actiongroup, 1)
+        
+        actiongroup = gtk.ActionGroup('Edit')
+        actiongroup.add_actions([
+            ('Preferences', gtk.STOCK_PREFERENCES, _('_Preferences'), None,
+             _('Display preferences'), self._on_button_prefs_clicked),
+        ])
+        uimanager.insert_action_group(actiongroup, 2)
+        
+        actiongroup = gtk.ActionGroup('View')
+        actiongroup.add_toggle_actions([
+            ('Channels', None, _('Channels'), None,
+             _('View/Hide channels'), self._on_view_channels_clicked),
+            ('Toolbar', None, _('Toolbar'), None,
+             _('View/Hide toolbar'), self._on_view_toolbar_clicked),
+        ])
+        action = actiongroup.get_action('Toolbar')
+        action.set_active(True)
+        action = actiongroup.get_action('Channels')
+        action.set_active(True)
+        uimanager.insert_action_group(actiongroup, 3)
+        
+        actiongroup = gtk.ActionGroup('Edit')
+        actiongroup.add_actions([
+            ('About', gtk.STOCK_ABOUT, _('_About'), None,
+             _('Display informations about the program'),
+             self._on_about_clicked),
+        ])
+        uimanager.insert_action_group(actiongroup, 4)
+
+        # Add a UI description
+        uimanager.add_ui_from_string(ui)
+        
+        # Create a MenuBar
+        menubar = uimanager.get_widget('/MenuBar')
+        menubar.show()
+        self.vbox_outer.pack_start(menubar, False)
+        
+    def __create_toolbar(self):
+        self.toolbar = gtk.Toolbar()
+        self.toolbar.show()
+        self.vbox_outer.pack_start(self.toolbar, False)
+        
+        edit_image = gtk.image_new_from_stock(gtk.STOCK_SAVE, gtk.ICON_SIZE_SMALL_TOOLBAR)
         edit_image.show()
-        self.button_display_timers = gtk.ToolButton(icon_widget=edit_image, label=_("Edit Recordings"))
+        self.button_display_timers = gtk.ToolButton(icon_widget=edit_image, label=_("Timers"))
         self.button_display_timers.set_sensitive(False)
         self.button_display_timers.connect("clicked", self._on_button_display_timers_clicked)
-        self.button_display_timers.set_tooltip_markup(_("View scheduled recordings"))
+        self.button_display_timers.set_tooltip_markup(_("View and add scheduled recordings"))
         self.button_display_timers.show()
-        toolbar.insert(self.button_display_timers, 0)
+        self.toolbar.insert(self.button_display_timers, 0)
         
         sep = gtk.SeparatorToolItem()
         sep.show()
-        toolbar.insert(sep, 1)
+        self.toolbar.insert(sep, 1)
         
         self.button_prefs = gtk.ToolButton(gtk.STOCK_PREFERENCES)
         self.button_prefs.connect("clicked", self._on_button_prefs_clicked)
         self.button_prefs.set_tooltip_markup(_("Manage devices"))
         self.button_prefs.show()
-        toolbar.insert(self.button_prefs, 2)
+        self.toolbar.insert(self.button_prefs, 2)
          
     def get_device_groups(self):
         for group in self.manager.get_registered_device_groups():
@@ -168,4 +252,20 @@ class ControlCenterWindow(gtk.Window):
         prefs = Preferences(self.manager, self)
         prefs.run()
         prefs.destroy()
+        
+    def _on_view_channels_clicked(self, action):
+        if self.vbox_left:
+            if action.get_active():
+                self.vbox_left.show()
+            else:
+                self.vbox_left.hide()
+        
+    def _on_view_toolbar_clicked(self, action):
+        if self.toolbar:
+            if action.get_active():
+                self.toolbar.show()
+            else:
+                self.toolbar.hide()
 
+    def _on_about_clicked(self, action):
+        pass
