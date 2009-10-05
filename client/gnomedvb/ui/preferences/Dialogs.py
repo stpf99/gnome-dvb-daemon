4970235962863cc957d2d8e89b2b5228683fd451
Sebastian Pölsterl <sebp@k-d-w.org>
2009-10-05 22:58:22 +0200

    Import AlignedLabel from the correct module

--- client/gnomedvb/ui/preferences/Dialogs.py
+++ client/gnomedvb/ui/preferences/Dialogs.py
@@ -19,7 +19,7 @@
 import gtk
 import gobject
 from gettext import gettext as _
-from gnomedvb.ui.preferences.Frame import AlignedLabel
+from gnomedvb.ui.widgets.Frame import AlignedLabel
 
 __all__ = ["AddToGroupDialog", "NewGroupDialog", "EditGroupDialog"]
 
