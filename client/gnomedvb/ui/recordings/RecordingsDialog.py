d4d5c08787c7a49ab8357bae62655269a40ea51c
Sebastian Pölsterl <sebp@k-d-w.org>
2009-12-01 20:32:49 +0100

    Made close button default widget

--- client/gnomedvb/ui/recordings/RecordingsDialog.py
+++ client/gnomedvb/ui/recordings/RecordingsDialog.py
@@ -29,12 +29,14 @@ class RecordingsDialog(gtk.Dialog):
     def __init__(self, parent=None):
         gtk.Dialog.__init__(self, title=_("Recordings"),
             parent=parent,
-            flags=gtk.DIALOG_MODAL | gtk.DIALOG_DESTROY_WITH_PARENT,
-            buttons=(gtk.STOCK_CLOSE, gtk.RESPONSE_CLOSE))
+            flags=gtk.DIALOG_MODAL | gtk.DIALOG_DESTROY_WITH_PARENT)
             
         self.set_size_request(600, 400)
         self.set_has_separator(False)
         self.vbox.set_spacing(12)
+        
+        close_button = self.add_button(gtk.STOCK_CLOSE, gtk.RESPONSE_CLOSE)
+        close_button.grab_default()
             
         hbox_main = gtk.HBox(spacing=12)
         hbox_main.set_border_width(6)
