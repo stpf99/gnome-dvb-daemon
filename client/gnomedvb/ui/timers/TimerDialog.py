931fa4e777259ce90f9d46ab7e026d6577e14017
Sebastian Pölsterl <sebp@k-d-w.org>
2010-01-30 23:02:32 +0100

    Use DateTimeBox widget to edit start time. Added option to provide channel, start time and duration in the constructor.

--- client/gnomedvb/ui/timers/TimerDialog.py
+++ client/gnomedvb/ui/timers/TimerDialog.py
@@ -23,24 +23,28 @@ from gnomedvb.ui.timers.CalendarDialog import CalendarDialog
 from gnomedvb.ui.widgets.ChannelsStore import ChannelsStore
 from gnomedvb.ui.widgets.ChannelsView import ChannelsView
 from gnomedvb.ui.widgets.Frame import TextFieldLabel
+from gnomedvb.ui.widgets.DateTime import DateTimeBox
 
 class TimerDialog(gtk.Dialog):
 
-    def __init__(self, parent, device_group):
+    def __init__(self, parent, device_group, channel=None,
+            starttime=datetime.datetime.now(), duration=60):
         """
         @param parent: Parent window
         @type parent: gtk.Window
         @param device_group: DeviceGroup instance
         """
-        gtk.Dialog.__init__(self, title=_("Add Timer"), parent=parent,
+        gtk.Dialog.__init__(self, parent=parent,
                 flags=gtk.DIALOG_MODAL | gtk.DIALOG_DESTROY_WITH_PARENT)
-        
+
+        self.set_default_size(320, 200)
+
         self.device_group = device_group
-        self._start_date = None
+        self.date_valid = False
+        self.allowed_delta = datetime.timedelta(hours=1)
         
         self.add_button(gtk.STOCK_CANCEL, gtk.RESPONSE_REJECT)
         self.ok_button = self.add_button(gtk.STOCK_OK, gtk.RESPONSE_ACCEPT)
-        self.ok_button.set_sensitive(False)
         
         self.set_has_separator(False)
         self.set_border_width(5)
@@ -53,22 +57,38 @@ class TimerDialog(gtk.Dialog):
                          
         label_channel = TextFieldLabel()
         label = label_channel.get_label()
-        label.set_markup_with_mnemonic(_("_Channel:"))
         table.attach(label_channel, 0, 1, 0, 1, gtk.FILL, gtk.FILL)
         
-        self.channels = ChannelsStore(device_group)
+        if channel == None:
+            self.channel_selected = False
+            self.set_title(_("Add Timer"))
+            self.ok_button.set_sensitive(False)
+
+            label.set_markup_with_mnemonic(_("_Channel:"))
+            self.channels = ChannelsStore(device_group)
         
-        scrolledchannels = gtk.ScrolledWindow()
-        scrolledchannels.set_policy(gtk.POLICY_AUTOMATIC, gtk.POLICY_AUTOMATIC)
-        scrolledchannels.set_shadow_type(gtk.SHADOW_ETCHED_IN)
-        table.attach(scrolledchannels, 0, 2, 1, 2)
-        
-        self.channelsview = ChannelsView(self.channels)
-        self.channelsview.set_headers_visible(False)
-        self.channelsview.get_selection().connect("changed",
-            self._on_channel_changed)
-        scrolledchannels.add(self.channelsview)
-        label.set_mnemonic_widget(self.channelsview)
+            scrolledchannels = gtk.ScrolledWindow()
+            scrolledchannels.set_policy(gtk.POLICY_AUTOMATIC, gtk.POLICY_AUTOMATIC)
+            scrolledchannels.set_shadow_type(gtk.SHADOW_ETCHED_IN)
+            table.attach(scrolledchannels, 0, 2, 1, 2)
+            
+            self.channelsview = ChannelsView(self.channels)
+            self.channelsview.set_headers_visible(False)
+            self.channelsview.get_selection().connect("changed",
+                self._on_channel_changed)
+            scrolledchannels.add(self.channelsview)
+            label.set_mnemonic_widget(self.channelsview)
+            self.channelsview.grab_focus()
+        else:
+            self.channel_selected = True
+            self.set_title(_("Edit Timer"))
+            self.ok_button.set_sensitive(True)
+
+            label.set_text(_("Channel:"))
+            self.channels = None
+            self.channelsview = None
+            channel_label = TextFieldLabel(channel)
+            table.attach(channel_label, 1, 2, 0, 1, yoptions=gtk.FILL)
         
         label_start = TextFieldLabel()
         label = label_start.get_label()
@@ -78,32 +98,10 @@ class TimerDialog(gtk.Dialog):
         hbox = gtk.HBox(spacing=6)
         table.attach(hbox, 1, 2, 2, 3, yoptions=0)
         
-        self.entry = gtk.Entry()
-        self.entry.set_editable(False)
-        self.entry.set_width_chars(10)
-        hbox.pack_start(self.entry)
-        label.set_mnemonic_widget(self.entry)
-        
-        calendar_button = gtk.Button(_("Pick date"))
-        calendar_button.connect("clicked", self._on_calendar_button_clicked)
-        hbox.pack_start(calendar_button)
-        
-        self.hour = gtk.SpinButton()
-        self.hour.set_range(0, 23)
-        self.hour.set_increments(1, 3)
-        self.hour.set_wrap(True)
-        self.hour.set_width_chars(2)
-        hbox.pack_start(self.hour)
-        
-        hour_minute_seperator = gtk.Label(":")
-        hbox.pack_start(hour_minute_seperator)
-        
-        self.minute = gtk.SpinButton()
-        self.minute.set_range(0, 59)
-        self.minute.set_increments(1, 15)
-        self.minute.set_wrap(True)
-        self.minute.set_width_chars(2)
-        hbox.pack_start(self.minute)
+        self.datetime_box = DateTimeBox(starttime)
+        self.datetime_box.connect("changed", self._on_datetime_changed)
+        hbox.pack_start(self.datetime_box)
+        label.set_mnemonic_widget(self.datetime_box)
         
         label_duration = TextFieldLabel()
         label = label_duration.get_label()
@@ -124,52 +122,54 @@ class TimerDialog(gtk.Dialog):
         minutes_label = TextFieldLabel(_("minutes"))
         duration_hbox.pack_start(minutes_label)
         
-        self._set_default_time_and_date()
+        self.set_start_time(starttime)
+        self.set_duration(duration)
         
         table.show_all()
-        self.channelsview.grab_focus()
-      
+
     def get_duration(self):
         return self.duration.get_value_as_int()
+
+    def set_duration(self, minutes):
+        self.duration.set_value(minutes)
         
     def get_start_time(self):
-        start = []
-        
-        for i in range(3):
-            start.append(self._start_date[i])
-        
-        start.append(self.hour.get_value_as_int())
-        start.append(self.minute.get_value_as_int())
-        
-        return start
+        return self.datetime_box.get_date_and_time()
+
+    def set_start_time(self, time):
+        self.datetime_box.set_date_and_time(time.year, time.month, time.day,
+            time.hour, time.minute)
         
     def get_channel(self):
+        if self.channelsview == None:
+            return None
         model, aiter = self.channelsview.get_selection().get_selected()
         if aiter != None:
             return model[aiter][1]
         else:
             return None
-        
-    def _set_default_time_and_date(self):
-        current = datetime.datetime.now()
-        self._set_date(current.year, current.month, current.day)
-        
-        self.hour.set_value(current.hour)
-        self.minute.set_value(current.minute)
-        
-    def _set_date(self, year, month, day):
-        self._start_date = (year, month, day)
-        self.entry.set_text("%04d-%02d-%02d" % (year, month, day))
-        
-    def _on_calendar_button_clicked(self, button):
-        d = CalendarDialog(self)
-        if (d.run() == gtk.RESPONSE_ACCEPT):
-            date = d.get_date()
-            self._set_date(date[0], date[1]+1, date[2])
-        
-        d.destroy()
+
+    def set_time_and_date_editable(self, val):
+        self.datetime_box.set_editable(val)
         
     def _on_channel_changed(self, treeselection):
         model, aiter = treeselection.get_selected()
-        self.ok_button.set_sensitive(aiter != None)
+        self.channel_selected = (aiter != None)
+        self._update_sensivity()
+
+    def _on_datetime_changed(self, widget, year, mon, day, hour, minute):
+        dt_new = datetime.datetime(year, mon, day, hour, minute)
+        dt_now = datetime.datetime.now()
+        delta = dt_new - dt_now
+        # Check if delta is negative
+        if delta < datetime.timedelta():
+            self.date_valid = abs(delta) <= self.allowed_delta
+        else:
+            self.date_valid = True
+        self._update_sensivity()
+
+    def _update_sensivity(self):
+        status = self.date_valid and self.channel_selected
+        self.ok_button.set_sensitive(status)
+        self.datetime_box.mark_valid(self.date_valid)
 
