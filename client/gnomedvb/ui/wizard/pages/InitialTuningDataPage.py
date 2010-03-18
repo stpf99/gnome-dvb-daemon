7bdc3d6904f0af277d3578b8ba8e45fc11de60fd
Sebastian Pölsterl <sebp@k-d-w.org>
2010-03-18 19:50:52 +0100

    Use TreeView to display satellites, antennas and cable providers. Added mnemonics.

--- client/gnomedvb/ui/wizard/pages/InitialTuningDataPage.py
+++ client/gnomedvb/ui/wizard/pages/InitialTuningDataPage.py
@@ -24,6 +24,7 @@ import gettext
 import locale
 from gettext import gettext as _
 from gnomedvb.ui.wizard.pages.BasePage import BasePage
+from gnomedvb.ui.widgets.Frame import TextFieldLabel
 
 DVB_APPS_DIRS = ("/usr/share/dvb",
                  "/usr/share/dvb-apps",
@@ -90,7 +91,7 @@ class InitialTuningDataPage(BasePage):
             self.__page_title = _("Country and antenna selection")
         elif info["type"] == "DVB-S":
             self.setup_dvb_s()
-            self.__page_title = _("Country and antenna selection")
+            self.__page_title = _("Satellite selection")
         elif info["type"] == "DVB-C":
             self.setup_dvb_c()
             self.__page_title = _("Country and provider selection")
@@ -104,9 +105,9 @@ class InitialTuningDataPage(BasePage):
         return self.__tuning_data
         
     def _create_table(self):
-        self.table = gtk.Table(rows=5, columns=2)
+        self.table = gtk.Table(rows=4, columns=2)
         self.table.set_row_spacings(6)
-        self.table.set_col_spacings(3)
+        self.table.set_col_spacings(18)
         self.table.show()
         self.pack_start(self.table)
 
@@ -134,7 +135,7 @@ class InitialTuningDataPage(BasePage):
             _("However, searching for channels will take considerably longer this way."))
         self._label.set_markup(text)
     
-        self.providers_combo = None
+        self.providers_view = None
         
         countries = {self.NOT_LISTED: _("Not listed")}
         country_codes = ("at", "au", "be", "ch", "cz", "de", "dk", "es", "fi", "fr",
@@ -146,10 +147,11 @@ class InitialTuningDataPage(BasePage):
         
         self._create_table()
         
-        country = gtk.Label()
-        country.set_markup("<b>%s</b>" % _("Country:"))
+        country = TextFieldLabel()
+        label = country.get_label()
+        label.set_markup_with_mnemonic(_("_Country:"))
         country.show()
-        self.table.attach(country, 0, 1, 0, 1, yoptions=0)
+        self.table.attach(country, 0, 1, 0, 1, yoptions=0, xoptions=gtk.FILL)
 
         # name, code    
         self.countries = gtk.ListStore(str, str)
@@ -167,45 +169,45 @@ class InitialTuningDataPage(BasePage):
         self.country_combo.show()
         self.table.attach(self.country_combo, 1, 2, 0, 1, yoptions=0)
         self.country_combo.set_active(0)
+        label.set_mnemonic_widget(self.country_combo)
         
-        self.providers_label = gtk.Label()
-        self.providers_label.set_markup("<b>%s</b>" % _("Antenna:"))
-        self.providers_label.show()
-        self.table.attach(self.providers_label, 0, 1, 1, 2, yoptions=0)
+        providers = TextFieldLabel()
+        label = providers.get_label()
+        label.set_markup_with_mnemonic(_("_Antenna:"))
+        providers.show()
+        self.table.attach(providers, 0, 1, 1, 2, yoptions=0, xoptions=gtk.FILL)
         
         self.providers = gtk.ListStore(str, str)
         self.providers.set_sort_column_id(0, gtk.SORT_ASCENDING)
         self.providers.set_sort_func(0, self.combobox_sort_func)
         
-        self.providers_combo = gtk.ComboBox(self.providers)
-        self.providers_combo.connect('changed', self.on_providers_changed)
-        cell = gtk.CellRendererText()
-        self.providers_combo.pack_start(cell)
-        self.providers_combo.add_attribute(cell, "text", 0)
-        self.providers_combo.show()
-        self.table.attach(self.providers_combo, 1, 2, 1, 2, yoptions=0)
-        self.providers_combo.set_sensitive(False)
+        self.providers_view, scrolledview = self._create_providers_treeview(
+            self.providers, _("Antenna"))
+        self.providers_view.get_selection().connect('changed',
+            self.on_providers_changed)
+        label.set_mnemonic_widget(self.providers_view)
         
+        self.table.attach(scrolledview, 0, 2, 2, 3)
+        
+        self.providers_view.set_sensitive(False)
+   
     def setup_dvb_s(self):
-        hbox = gtk.HBox(spacing=6)
-        hbox.show()
-        self.pack_start(hbox, False)
         
-        satellite = gtk.Label()
-        satellite.set_markup("<b>%s</b>" % _("Satellite:"))
+        satellite = TextFieldLabel()
+        label = satellite.get_label()
+        label.set_markup_with_mnemonic(_("_Satellite:"))
         satellite.show()
-        hbox.pack_start(satellite, False, False, 0)
+        self.pack_start(satellite, False, False, 0)
         
         self.satellites = gtk.ListStore(str, str)
         self.satellites.set_sort_column_id(0, gtk.SORT_ASCENDING)
         
-        self.satellite_combo = gtk.ComboBox(self.satellites)
-        self.satellite_combo.connect("changed", self.on_satellite_changed)
-        cell = gtk.CellRendererText()
-        self.satellite_combo.pack_start(cell)
-        self.satellite_combo.add_attribute(cell, "text", 0)
-        self.satellite_combo.show()
-        hbox.pack_start(self.satellite_combo, False, False, 0)
+        self.satellite_view, scrolledview = self._create_providers_treeview(
+            self.satellites, _("Satellite"))
+        self.satellite_view.get_selection().connect("changed",
+            self.on_satellite_changed)
+        label.set_mnemonic_widget(self.satellite_view)
+        self.pack_start(scrolledview)
         
         self.read_satellites()
         
@@ -218,10 +220,11 @@ class InitialTuningDataPage(BasePage):
             
         self._create_table()
             
-        country = gtk.Label()
-        country.set_markup("<b>%s</b>" % _("Country:"))
+        country = TextFieldLabel()
+        label = country.get_label()
+        label.set_markup_with_mnemonic(_("_Country:"))
         country.show()
-        self.table.attach(country, 0, 1, 0, 1, yoptions=0)
+        self.table.attach(country, 0, 1, 0, 1, yoptions=0, xoptions=gtk.FILL)
     
         self.countries = gtk.ListStore(str, str)
         self.countries.set_sort_column_id(0, gtk.SORT_ASCENDING)
@@ -237,24 +240,44 @@ class InitialTuningDataPage(BasePage):
         self.country_combo.add_attribute(cell, "text", 0)
         self.country_combo.show()
         self.table.attach(self.country_combo, 1, 2, 0, 1, yoptions=0)
+        label.set_mnemonic_widget(self.country_combo)
         
-        self.providers_label = gtk.Label()
-        self.providers_label.set_markup("<b>%s</b>" % _("Providers:"))
-        self.providers_label.show()
-        self.table.attach(self.providers_label, 0, 1, 1, 2, yoptions=0)
+        providers = TextFieldLabel()
+        label = providers.get_label()
+        label.set_markup_with_mnemonic(_("_Providers:"))
+        providers.show()
+        self.table.attach(providers, 0, 1, 1, 2, yoptions=0, xoptions=gtk.FILL)
         
         self.providers = gtk.ListStore(str, str)
         self.providers.set_sort_column_id(0, gtk.SORT_ASCENDING)
         
-        self.providers_combo = gtk.ComboBox(self.providers)
-        self.providers_combo.connect('changed', self.on_providers_changed)
+        self.providers_view, scrolledview = self._create_providers_treeview(
+            self.providers, _("Provider"))
+        self.providers_view.get_selection().connect('changed',
+            self.on_providers_changed)
+        label.set_mnemonic_widget(self.providers_view)
+        
+        self.table.attach(scrolledview, 0, 2, 2, 3)
+        self.providers_view.set_sensitive(False)
+         
+    def _create_providers_treeview(self, providers, col_name):
+        providers_view = gtk.TreeView(providers)
+        providers_view.set_headers_visible(False)
+        col = gtk.TreeViewColumn(col_name)
         cell = gtk.CellRendererText()
-        self.providers_combo.pack_start(cell)
-        self.providers_combo.add_attribute(cell, "text", 0)
-        self.providers_combo.show()
-        self.table.attach(self.providers_combo, 1, 2, 1, 2, yoptions=0)
-        self.providers_combo.set_sensitive(False)
-    
+        col.pack_start(cell)
+        col.add_attribute(cell, "markup", 0)
+        providers_view.append_column(col)
+        providers_view.show()
+        
+        scrolledview= gtk.ScrolledWindow()
+        scrolledview.add(providers_view)
+        scrolledview.set_policy(gtk.POLICY_NEVER, gtk.POLICY_AUTOMATIC)
+        scrolledview.set_shadow_type(gtk.SHADOW_ETCHED_IN)
+        scrolledview.show()
+        
+        return providers_view, scrolledview
+        
     def on_country_changed(self, combo, directory):
         aiter = combo.get_active_iter()
         
@@ -262,11 +285,13 @@ class InitialTuningDataPage(BasePage):
             selected_country = self.countries[aiter][1]
     
             if selected_country == self.NOT_LISTED:
-                if self.providers_combo:
-                    self.providers_combo.set_sensitive(False)
+                if self.providers_view:
+                    self.providers_view.set_sensitive(False)
             else:
                 self.providers.clear()
-                self.providers.append([_("Don't know"), self.NOT_LISTED])
+                # Only DVB-T has bruteforce scan
+                if self.__adapter_info["type"] == "DVB-T":
+                    self.providers.append([_("Don't know"), self.NOT_LISTED])
                 for d in DVB_APPS_DIRS:
                     if os.access(d, os.F_OK | os.R_OK):
                         for f in os.listdir(os.path.join(d, directory)):
@@ -275,12 +300,13 @@ class InitialTuningDataPage(BasePage):
                             if country == selected_country:
                                 self.providers.append([city, os.path.join(d, directory, f)])
             
-                self.providers_combo.set_sensitive(True)
-                self.providers_combo.set_active(0)
+                self.providers_view.set_sensitive(True)
+                first_iter = self.providers.get_iter_first()
+                self.providers_view.get_selection().select_iter(first_iter)
             self.emit("finished", True)
         
-    def on_providers_changed(self, combo):
-        aiter = combo.get_active_iter()
+    def on_providers_changed(self, selection):
+        model, aiter = selection.get_selected()
         
         if aiter != None:
             self.__tuning_data = self.providers[aiter][1]
@@ -292,8 +318,8 @@ class InitialTuningDataPage(BasePage):
                 for f in os.listdir(os.path.join(d, 'dvb-s')):
                     self.satellites.append([f, os.path.join(d, 'dvb-s', f)])
                         
-    def on_satellite_changed(self, combo):
-        aiter = combo.get_active_iter()
+    def on_satellite_changed(self, selection):
+        model, aiter = selection.get_selected()
         
         if aiter != None:
             self.__tuning_data = self.satellites[aiter][1]
@@ -302,7 +328,7 @@ class InitialTuningDataPage(BasePage):
     def on_scan_all_toggled(self, checkbutton):
         state = not checkbutton.get_active()
         self.country_combo.set_sensitive(state)
-        self.providers_combo.set_sensitive(state)
+        self.providers_view.set_sensitive(state)
         self.add_brute_force_scan()
         self.emit("finished", not state)
     
