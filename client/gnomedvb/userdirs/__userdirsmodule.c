88bbcd189c87e949352c1660ba7a35b58dcf06ba
Sebastian Pölsterl <sebp@k-d-w.org>
2010-04-09 17:26:32 +0200

    Convert XDG user dir path utf8

--- client/gnomedvb/userdirs/__userdirsmodule.c
+++ client/gnomedvb/userdirs/__userdirsmodule.c
@@ -14,35 +14,49 @@ static void userdirs_register_constants (PyObject *self);
 /* Function Mapping Table */
 static PyMethodDef py__userdirs_functions[] =
 {
-	{ "get_xdg_user_dir", userdirs_get_xdg_user_dir, 0, "" },
-	{ NULL, NULL, 0, NULL }
+    { "get_xdg_user_dir", userdirs_get_xdg_user_dir, 0, "" },
+    { NULL, NULL, 0, NULL }
 };
 
 PyMODINIT_FUNC
 init__userdirs (void)
 {
-	PyObject* m;
+    PyObject* m;
 
-	m = Py_InitModule ("__userdirs", py__userdirs_functions);
-	userdirs_register_constants (m);
+    m = Py_InitModule ("__userdirs", py__userdirs_functions);
+    userdirs_register_constants (m);
 }
 	
 static PyObject *
 userdirs_get_xdg_user_dir (PyObject *self, PyObject *directory)
 {
-    if (!PyInt_Check (directory)) {
-    	PyErr_SetString (PyExc_TypeError, "The first argument must be an integer");
-        return NULL;
-    } else {
-        const gchar *dir = g_get_user_special_dir ((GUserDirectory) PyInt_AsLong (directory));
+    const gchar *dir;
+    gchar *locale_dir;
+    PyObject *locale_dir_obj;
+    GError *error = NULL;
 
-        if (dir) {
-            return PyString_FromString (dir);
-        } else {
-        	Py_INCREF (Py_None);
-        	return Py_None;
-        }
+    if (!PyInt_Check (directory)) {
+        PyErr_SetString (PyExc_TypeError, "The first argument must be an integer");
+        return NULL;
     }
+
+    dir = g_get_user_special_dir ((GUserDirectory) PyInt_AsLong (directory));
+    if (!dir) {
+        Py_INCREF (Py_None);
+        return Py_None;
+    }
+
+    locale_dir = g_filename_to_utf8 (dir, -1, NULL, NULL, &error);
+    if (error != NULL) {
+        PyErr_SetString (PyExc_RuntimeError, error->message);
+        g_error_free (error);
+        return NULL;
+    }
+
+    locale_dir_obj = PyString_FromString (locale_dir);
+    g_free (locale_dir);
+
+    return locale_dir_obj;
 }
 
 static void
