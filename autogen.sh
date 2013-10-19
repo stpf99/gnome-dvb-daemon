6e5c9e1789426c44757075ae4689798103c95ef5
Sebastian Pölsterl <sebp@k-d-w.org>
2013-10-19 12:23:57 +0200

    Fix i18n build error
    https://bugzilla.gnome.org/show_bug.cgi?id=710087

--- autogen.sh
+++ autogen.sh
@@ -6,5 +6,19 @@ test -z "$srcdir" && srcdir=.
 
 PKG_NAME="gnome-dvb-daemon"
 
+(test -f $srcdir/configure.ac) || {
+    echo -n "**Error**: Directory "\`$srcdir\'" does not look like the"
+    echo " top-level $PKG_NAME directory"
+    exit 1
+}
+
+which gnome-autogen.sh || {
+	echo "You need to install gnome-common from the GNOME git"
+	exit 1
+}
+
 touch ChangeLog
-. gnome-autogen.sh
+
+REQUIRED_AUTOMAKE_VERSION=1.11
+REQUIRED_AUTOCONF_VERSION=2.63.2
+REQUIRED_INTLTOOL_VERSION=0.40.0 . gnome-autogen.sh "$@"
